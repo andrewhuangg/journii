8bb4027b83c9a17ce357eca6ab9f0c78d9da2608
Andrew <drewhuangg@gmail.com>
2020-10-02 16:02:50 -0700

    create basic login and register forms, create store and reducers

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-const Register = () => <div>register</div>;
+  const { name, email, password, confirmPassword } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>Create Your Account</p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input type='text' placeholder='Name' name='name' value={name} onChange={(e) => onChange(e)} required />
+        </div>
+        <div className='form-group'>
+          <input type='email' placeholder='Email Address' name='email' value={email} onChange={(e) => onChange(e)} />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='confirmPassword'
+            value={confirmPassword}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Sign In</Link>
+      </p>
+    </>
+  );
+};
 
 export default Register;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Landing = () => <section class='landing'>landing</section>;
+const Landing = () => (
+  <section className='landing'>
+    <div className='buttons'>
+      <Link to='/register'>register</Link>
+      <Link to='/login'>login</Link>
+    </div>
+  </section>
+);
 
 export default Landing;
