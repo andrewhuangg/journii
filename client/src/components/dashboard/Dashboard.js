5a85ff52ac7b92f6310ba4a118a702ccda2b209d
Andrew <drewhuangg@gmail.com>
2020-11-18 21:51:32 -0800

    mobile first dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import React, { useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 // import { useDispatch, useSelector } from 'react-redux';
@@ -10,29 +11,30 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum."
-      </p>
+      <section className='showcase'>
+        <div className='showcase__video-container'>
+          <video
+            autoPlay
+            loop
+            muted
+            src={'https://journii-dev.s3-us-west-1.amazonaws.com/journii_hero_video.mp4'}
+          />
+        </div>
+        <div className='showcase__content'>
+          <h1>journii</h1>
+          <p>It never stops.</p> {/*  quote generator here.*/}
+        </div>
+      </section>
+
+      <section className='hero'>
+        <div className='hero__image'></div>
+        <div className='hero__text'>
+          <p>The journey never stops.</p> {/*  quote generator here.*/}
+          <Link to='#' className='button hero__cta'>
+            Lets get started
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -24,21 +24,21 @@ const Header = ({ history }) => {
     if (successCreate) {
       dispatch(getOwnProfileDetails());
     }
-  }, [dispatch, user, userInfo, successCreate, history]);
+  }, [dispatch, user, userInfo, successCreate, profileInfo, history]);
 
   const logoutHandler = () => {
     dispatch(logout());
   };
 
-  const btnHamburger = () => {
+  const mobileHamburger = () => {
     const header = document.querySelector('.header');
     const body = document.querySelector('body');
-    const fadeElems = document.querySelectorAll('.has-fade');
+    const fadeElems = document.querySelectorAll('.mobile-has-fade');
 
-    if (header.classList.contains('open')) {
+    if (header.classList.contains('mobile-open')) {
       //Close Hamburger Menu
       body.classList.remove('noscroll');
-      header.classList.remove('open');
+      header.classList.remove('mobile-open');
       fadeElems.forEach((el) => {
         el.classList.remove('fade-in');
         el.classList.add('fade-out');
@@ -46,7 +46,7 @@ const Header = ({ history }) => {
     } else {
       //Open Hamburger Menu
       body.classList.add('noscroll');
-      header.classList.add('open');
+      header.classList.add('mobile-open');
       fadeElems.forEach((el) => {
         el.classList.remove('fade-out');
         el.classList.add('fade-in');
@@ -54,9 +54,32 @@ const Header = ({ history }) => {
     }
   };
 
+  const hamburger = () => {
+    const header = document.querySelector('.header');
+    const body = document.querySelector('body');
+    const slideElems = document.querySelectorAll('.has-fade');
+    if (header.classList.contains('open')) {
+      //Close Hamburger Menu
+      body.classList.remove('noscroll');
+      header.classList.remove('open');
+      slideElems.forEach((el) => {
+        el.classList.remove('slide-in');
+        el.classList.add('slide-out');
+      });
+    } else {
+      //Open Hamburger Menu
+      body.classList.add('noscroll');
+      header.classList.add('open');
+      slideElems.forEach((el) => {
+        el.classList.remove('slide-out');
+        el.classList.add('slide-in');
+      });
+    }
+  };
+
   const headerLinks = (
     <>
-      <Link to='/Home'>Home</Link>
+      <Link to='/Dashboard'>Home</Link>
       <Link to='/posts'>Posts</Link>
       <Link to='/profiles'>Profiles</Link>
     </>
@@ -64,14 +87,19 @@ const Header = ({ history }) => {
 
   return (
     <header className='header'>
-      <div className='overlay has-fade'></div>
+      <div className='overlay has-fade mobile-has-fade'></div>
+
       <nav className='container flex flex-jc-sb flex-ai-c'>
         <a href='/' className='header__logo'>
           {/* <img alt='journii'>journii</img> */}
           <div>journii logo goes here</div>
         </a>
 
-        <a id='btnHamburger' className='header__toggle hide-for-desktop' onClick={btnHamburger}>
+        <a
+          id='mobileHamburger'
+          className='header__toggle hide-for-desktop'
+          onClick={mobileHamburger}
+        >
           <span></span>
           <span></span>
           <span></span>
@@ -79,36 +107,41 @@ const Header = ({ history }) => {
 
         <div className='header__links hide-for-mobile'>{headerLinks}</div>
 
-        <div className='header__links hide-for-mobile'>
-          {userInfo ? (
-            <>
-              <Link to='/userinfo'>Update User</Link>
-              {!profileInfo && user && !user.ownProfile && (
-                <Link className='linkTag' to='/createprofile'>
-                  Create Profile
-                </Link>
-              )}
-              {profileInfo && user && user.ownProfile && (
-                <Link to='/editprofile'>Update Profile</Link>
-              )}
-            </>
-          ) : (
-            <>
-              <Link to='/register'>Register</Link>
-              <Link to='/login'>Login</Link>
-            </>
-          )}
-          {userInfo && <a onClick={logoutHandler}>Log out</a>}
-        </div>
-
         {userInfo && (
           <Link to='/createpost' className='button header__cta hide-for-mobile'>
             Create Post
           </Link>
         )}
+
+        <a id='btnHamburger' className='header__toggle hide-for-mobile' onClick={hamburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
       </nav>
 
-      <div className='header__menu has-fade'>{headerLinks}</div>
+      <div className='header__menu hide-for-mobile has-fade'>
+        {userInfo ? (
+          <>
+            <Link to='/userinfo'>Update User</Link>
+            {!profileInfo && user && !user.ownProfile ? (
+              <Link className='linkTag' to='/createprofile'>
+                Create Profile
+              </Link>
+            ) : (
+              <Link to='/editprofile'>Update Profile</Link>
+            )}
+          </>
+        ) : (
+          <>
+            <Link to='/register'>Register</Link>
+            <Link to='/login'>Login</Link>
+          </>
+        )}
+        {userInfo && <a onClick={logoutHandler}>Log out</a>}
+      </div>
+
+      <div className='header__mobile-menu mobile-has-fade'>{headerLinks}</div>
     </header>
   );
 };
