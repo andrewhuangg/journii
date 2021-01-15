3032fa525728369c33dab5600b8253307991d1c1
Andrew <drewhuangg@gmail.com>
2021-01-14 17:13:50 -0800

    minor tweaks to rating star size, columns to postlistcards

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,7 +4,7 @@ import Moment from 'react-moment';
 import Rating from '../layout/Rating';
 
 const PostItem = ({
-  post: { _id, text, title, image, rating, numReviews, likes, follows, comments, createdAt },
+  post: { _id, text, title, image, rating, numReviews, likes, comments, createdAt },
 }) => {
   const unsplashURL = 'https://source.unsplash.com/collection/289662/';
 
@@ -38,9 +38,6 @@ const PostItem = ({
         <div className='postItem__comments'>
           <i className='fas fa-comments' /> {comments.length > 0 && comments.length}
         </div>
-        <div className='postItem__follows'>
-          <i className='fas fa-users' /> {follows.length > 0 && follows.length}
-        </div>
         <div className='postItem__rating'>
           <Rating value={rating} text={`${numReviews}`} />
         </div>
@@ -53,47 +50,3 @@ const PostItem = ({
 };
 
 export default PostItem;
-
-/*
-<div>
-        {image && <Image src={image} alt={title} fluid />}
-        <h6>
-          <Link to={`posts/${_id}`}>{title}</Link>
-        </h6>
-        <p>{text}</p>
-        <p>
-          Publisher <Link to={`profiles/${user}`}>{name}</Link>
-        </p>
-        <p>
-          Posted on <Moment format='MM/DD/YYYY'>{createdAt}</Moment>
-        </p>
-
-        <ul>
-          <li>
-            <Link to={`posts/${_id}`}>likes {likes.length > 0 && <span>{likes.length}</span>}</Link>
-          </li>
-          <li>
-            <Link to={`posts/${_id}`}>
-              follows {follows.length > 0 && <span>{follows.length}</span>}
-            </Link>
-          </li>
-          <li>
-            <Link to={`posts/${_id}`}>
-              comments {comments.length > 0 && <span>{comments.length}</span>}
-            </Link>
-          </li>
-          <li>
-            <Link to={`posts/${_id}`}>reviews {numReviews > 0 && <span>{numReviews}</span>}</Link>
-          </li>
-        </ul>
-        {currentUserId === user && <p>{<Link to={`editpost/${_id}`}>Edit Post</Link>} </p>}
-        {currentUserId === user && (
-          <Button onClick={() => deleteHandler(_id)}>
-            <i className='fas fa-trash'></i>
-          </Button>
-        )}
-        <div>
-          <Rating value={rating} text={` ${numReviews} reviews`} />
-        </div>
-      </div>
-*/
